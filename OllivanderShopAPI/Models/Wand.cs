b37741a2992f8af7615c387cdac9122bbbf52cfd
Maks0s <mrmaksman01@mail.ru>
2023-08-21 14:27:12 +0300

    Wand model added

---
+++ OllivanderShopAPI/Models/Wand.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OllivandersShopAPI.Models
+{
+    public class Wand
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Core { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Wood { get; set; }
+        [Required]
+        [Range(6, 17)]
+        public decimal LengthInInches { get; set; }
+        public string? TrueOwner { get; set; }
+    }
+
+}
