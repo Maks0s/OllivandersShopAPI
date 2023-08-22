6b7e78e99f6de26301b3b4b05bee47325b27663d
Maks0s <mrmaksman01@mail.ru>
2023-08-22 11:43:34 +0300

    Fix LengthInInches And TrueOwner constraints and add to DB

--- OllivanderShopAPI/Migrations/20230822084034_AddLengthInInchesAndTrueOwnerConstraints.Designer.cs
+++ OllivanderShopAPI/Migrations/20230822084034_AddLengthInInchesAndTrueOwnerConstraints.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OllivandersShopAPI.Data;
 
@@ -10,9 +11,11 @@ using OllivandersShopAPI.Data;
 namespace OllivandersShopAPI.Migrations
 {
     [DbContext(typeof(OllivandersShopDbContext))]
-    partial class OllivandersShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230822084034_AddLengthInInchesAndTrueOwnerConstraints")]
+    partial class AddLengthInInchesAndTrueOwnerConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OllivanderShopAPI/Migrations/20230822084034_AddLengthInInchesAndTrueOwnerConstraints.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OllivandersShopAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLengthInInchesAndTrueOwnerConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Wood",
+                table: "Wands",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TrueOwner",
+                table: "Wands",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LengthInInches",
+                table: "Wands",
+                type: "decimal(4,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Wood",
+                table: "Wands",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TrueOwner",
+                table: "Wands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LengthInInches",
+                table: "Wands",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(4,2)");
+        }
+    }
+}
