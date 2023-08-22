f7b72726b54b58a27872b1aca5dbe9214f8c2a47
Maks0s <mrmaksman01@mail.ru>
2023-08-22 11:49:32 +0300

    Add GetAll and GetById endpoints to WandsController

---
+++ OllivanderShopAPI/Controllers/WandsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OllivandersShopAPI.Data;
+using OllivandersShopAPI.Mapper.Abstractions;
+using OllivandersShopAPI.Models;
+using OllivandersShopAPI.Models.DTO;
+using System.Net;
+
+namespace OllivandersShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WandsController : ControllerBase
+    {
+        private readonly ILogger<WandsController> _logger;
+        private readonly OllivandersShopDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public WandsController(ILogger<WandsController> logger, 
+            OllivandersShopDbContext dbContext,
+            IMapper mapper)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<GetWandDto>>> GetAllWands()
+        {
+            _logger.LogInformation("Getting all WANDS from DB");
+
+            var wands = await _dbContext.Wands.ToListAsync();
+
+            if(wands.Count == 0)
+            {
+                _logger.LogError("DB side error: DB is empty");
+
+                return Problem(title: "DB side error", detail: "DB is empty", statusCode: 500);
+            }
+
+            _logger.LogInformation("Map to DTO");
+
+            var dto = _mapper.MapToGetWandsDto(wands);
+
+            return Ok(dto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<GetWandDto>> GetWandById(int id)
+        {
+            _logger.LogInformation("Getting WAND from DB with id:{id}", id);
+
+            var wand = await _dbContext.Wands.FindAsync(id);
+
+            if(wand is null)
+            {
+                _logger.LogWarning("WAND with ID:{id} not found", id);
+
+                return NotFound($"WAND with ID:{id} not found");
+            }
+
+            _logger.LogInformation("Map to DTO");
+
+            var dto = _mapper.MapToGetWandDto(wand);
+
+            return Ok(dto);
+        }
+
+
+        
+    }
+}
