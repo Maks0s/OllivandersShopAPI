cd97a3f369715002f9e42796904db2b6ab30eb70
Maks0s <mrmaksman01@mail.ru>
2023-08-22 10:01:21 +0300

    Add Serilog

--- OllivanderShopAPI/Program.cs
+++ OllivanderShopAPI/Program.cs
@@ -1,32 +1,63 @@
 using Microsoft.EntityFrameworkCore;
 using OllivandersShopAPI.Data;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<OllivandersShopDbContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
-});
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    Log.Information("Started app building");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Host.UseSerilog((_, loggerConfig) =>
+    {
+        var jsonConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true, reloadOnChange: true)
+            .Build();
+
+        loggerConfig
+            .ReadFrom.Configuration(jsonConfig);
+    });
+
+    // Add services to the container.
+    builder.Services.AddDbContext<OllivandersShopDbContext>(options =>
+	{
+		options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+	});
+
+	builder.Services.AddControllers();
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Error(ex, "Crushed during the building");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
