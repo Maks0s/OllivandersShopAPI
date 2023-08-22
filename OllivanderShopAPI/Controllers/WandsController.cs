e6092681e95ee564d352ae21658607ea98beccd5
Maks0s <mrmaksman01@mail.ru>
2023-08-22 14:05:43 +0300

    Add Put endpoint to WandsController

--- OllivanderShopAPI/Controllers/WandsController.cs
+++ OllivanderShopAPI/Controllers/WandsController.cs
@@ -29,7 +29,6 @@ namespace OllivandersShopAPI.Controllers
         public async Task<ActionResult<ICollection<GetWandDto>>> GetAllWands()
         {
             _logger.LogInformation("Getting all WANDS from DB");
-
             var wands = await _dbContext.Wands.ToListAsync();
 
             if(wands.Count == 0)
@@ -40,7 +39,6 @@ namespace OllivandersShopAPI.Controllers
             }
 
             _logger.LogInformation("Map to DTO");
-
             var dto = _mapper.MapToGetWandsDto(wands);
 
             return Ok(dto);
@@ -51,18 +49,15 @@ namespace OllivandersShopAPI.Controllers
         public async Task<ActionResult<GetWandDto>> GetWandById(int id)
         {
             _logger.LogInformation("Getting WAND from DB with id:{id}", id);
-
             var wand = await _dbContext.Wands.FindAsync(id);
 
             if(wand is null)
             {
                 _logger.LogWarning("WAND with ID:{id} not found", id);
-
                 return NotFound($"WAND with ID:{id} not found");
             }
 
             _logger.LogInformation("Map to DTO");
-
             var dto = _mapper.MapToGetWandDto(wand);
 
             return Ok(dto);
@@ -73,24 +68,48 @@ namespace OllivandersShopAPI.Controllers
         {
             if(dto is null)
             {
-                _logger.LogWarning("Sent wand is null");
-
-                return BadRequest("Wand to add is NULL");
+                _logger.LogWarning("Sent WAND is NULL");
+                return BadRequest("WAND to add is NULL");
             }
 
             _logger.LogInformation("Map from DTO");
-
             var wandToAdd = _mapper.MapToWand(dto);
 
-            _logger.LogInformation("Add wand {@dto} to DB", dto);
-
+            _logger.LogInformation("Add WAND {@dto}", dto);
             await _dbContext.AddAsync(wandToAdd);
-
             await _dbContext.SaveChangesAsync();
 
             var responseDto = _mapper.MapToGetWandDto(wandToAdd);
 
             return CreatedAtAction(nameof(GetWandById), new { id = wandToAdd.Id}, responseDto);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult> PutWand(int id, [FromBody] PutWandDto dto)
+        {
+            var wandToUpdate = await _dbContext.Wands.FindAsync(id);
+
+            if(wandToUpdate is null)
+            {
+                _logger.LogWarning("WAND with ID:{id} not found", id);
+                return NotFound($"WAND with ID:{id} not found");
+            }
+            if (dto is null)
+            {
+                _logger.LogWarning("Sent WAND is NULL");
+                return BadRequest("WAND to update is NULL");
+            }
+
+            _logger.LogInformation("Map from DTO");
+            _mapper.MapToWandToUpdate(dto, wandToUpdate);
+            var responseDto = _mapper.MapToGetWandDto(wandToUpdate);
+
+            _logger.LogInformation("Update WAND {@responseDto}", responseDto);
+            _dbContext.Wands.Update(wandToUpdate);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
         
     }
