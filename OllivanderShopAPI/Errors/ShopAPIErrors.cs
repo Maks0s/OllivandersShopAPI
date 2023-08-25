f1c30bad2a60ebdaac419e7ef176b629a4230608
Maks0s <mrmaksman01@mail.ru>
2023-08-25 10:29:04 +0300

    Add ErrorOr<T> libriory and Replace exceptions on errors

---
+++ OllivanderShopAPI/Errors/ShopAPIErrors.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+
+namespace OllivandersShopAPI.Errors
+{
+    public static class ShopAPIErrors
+    {
+        public static Error NotFound(string message)
+        {
+            return Error.NotFound("The object you're looking for doesn't exist", message);
+        }
+
+        public static Error ServerSide(string message)
+        {
+            return Error.NotFound("Unexpected problems on the server side. Try again a little later", message);
+        }
+
+        public static Error IncorrectObjectSent(string message)
+        {
+            return Error.NotFound("The object you're sending is NULL or not correct", message);
+        }
+    }
+}
